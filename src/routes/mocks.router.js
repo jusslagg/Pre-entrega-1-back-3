1e56df1b6d00501547285bdab03eacde45de2335
unknown <you@example.comlagg312@gmail.com>
2025-05-25 12:39:16 -0300

    se agrega metodo post

--- src/routes/mocks.router.js
+++ src/routes/mocks.router.js
@@ -25,4 +25,27 @@ router.get('/mockingusers', (req, res) => {
   });
 });
 
+import generateMockUsers from '../utils/mocking.js';
+import User from '../dao/models/User.js';
+
+router.post('/generateData', async (req, res) => {
+  const { users } = req.body;
+
+  const generatedUsers = generateMockUsers(users || 10);
+
+  try {
+    await User.insertMany(generatedUsers);
+    res.json({
+      status: 'success',
+      message: `${generatedUsers.length} users generated and inserted into the database.`,
+    });
+  } catch (error) {
+    console.error('Error al insertar usuarios en la base de datos:', error);
+    res.status(500).json({
+      status: 'error',
+      message: 'Error al insertar usuarios en la base de datos.',
+    });
+  }
+});
+
 export default router;
